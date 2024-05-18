c97dba08419bf82a039f574360ccead1f1628fa8
Pranav <pranavparasar321@gmail.com>
2024-05-18 11:45:33 +0530

    adding blog enabled

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = 8000;
+app.use(express.static(path.resolve("./public")));
 const cookieParser = require("cookie-parser");
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+const Blog = require("./models/blog");
 const mongoose = require("mongoose");
 const checkForAuthCookie = require("./middlewares/auth");
 app.set("view engine", "ejs");
@@ -16,10 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthCookie("token"));
 //we'll need this user for showing login/logout in navbar
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
   res.render("home", {
     user: req.user,
+    blogs: allBlogs,
   });
 });
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 app.listen(PORT, () => console.log(`server started at PORT: ${PORT}`));
--- models/blog.js
+++ models/blog.js
@@ -11,9 +11,10 @@ const blogSchema = new mongoose.Schema(
     },
     coverImgUrl: {
       type: String,
+      // default: "/images/default.jpeg",->not ./public/... as we're using middleare in index.js to serve static files in public folder
     },
     createdBy: {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "user", //model name
     },
   },
